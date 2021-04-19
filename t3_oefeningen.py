6e589c0f63facb9f0506eeb7bdfdf20d674d35c0
Coen <weco110503@leerling.mosa-rt.be>
2021-04-19 14:24:58 +0200

    v.2

--- t3_oefeningen.py
+++ t3_oefeningen.py
@@ -1,12 +1,18 @@
-def bmi():
+def bmi(lengte, gewicht):
+    return gewicht/lengte**2
+
+def omtrek_cirkel():
     return
 
-def omtrek_cirkel(zijde):
-
 def oppervlakte_cirkel():
+    return
 
 def oppervlakte_vierkant(zijde):
+    return
 
 def volume_cilinder():
+    return
+
 
 def volume_kubus():
+    return
