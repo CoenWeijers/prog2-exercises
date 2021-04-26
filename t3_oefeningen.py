96146342b0522c91c6f391ed855a293e22f02ddd
Coen <weco110503@leerling.mosa-rt.be>
2021-04-26 12:22:25 +0200

    test code v.2 met formules

--- t3_oefeningen.py
+++ t3_oefeningen.py
@@ -1,24 +1,31 @@
 def bmi(gewicht, lengte):
     resultaat = gewicht/lengte**2
-    return
+    return resultaat
 
-def omtrek_cirkel():
-    resultaat =
-    return
+def omtrek_cirkel(r):
+    pi = 3.14
+    reslut = 2 * pi * r
+    return resultaat
 
-def oppervlakte_cirkel():
-    resultaat =
-    return
+def oppervlakte_cirkel(r):
+    pi = 3.14
+    resultaat = r**2*pi
+    return resultaat
 
 def oppervlakte_vierkant(zijde):
-    resultaat =
-    return
+    resultaat = zijde**2
+    return resultaat
 
-def volume_cilinder():
-    resultaat =
-    return
+def volume_cilinder(r, h):
+    pi = 3.14
+    oppervlakte_cirkel()
+    resultaat = pi*r**2*h
+
+    return resultaat
+
 
 
 def volume_kubus():
-    resultaat =
-    return
+
+    resultaat = 3
+    return resultaat
